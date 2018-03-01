e0308232dcaf86e6ac54d16277ce0588fef79b90
liuguiyangnwpu <liuguiyangnwpu@163.com>
2018-03-01 21:17:16 +0800

    清理冗余部分工程代码

--- trainer/ssd/__init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2009 IW.
-# All rights reserved.
-#
-# Author: liuguiyang <liuguiyangnwpu@gmail.com>
-# Date:   2017/12/18
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
